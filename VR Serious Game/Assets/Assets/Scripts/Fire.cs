c058921ba756f412e8328050f1d20d0dbb8c5fb2
Anthony Posselli <anthony.posselli@gmail.com>
2019-10-17 13:23:52 -0600

    Add a couple scripts for fires and the extinguisher along with some prefabs.

---
+++ VR Serious Game/Assets/Assets/Scripts/Fire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    private ParticleSystem[] m_Systems;
+
+    private void Start()
+    {
+        m_Systems = GetComponentsInChildren<ParticleSystem>();
+    }
+
+
+    public void Extinguish()
+    {
+        foreach (var system in m_Systems)
+        {
+            var emission = system.emission;
+            emission.enabled = false;
+        }
+    }
+}
