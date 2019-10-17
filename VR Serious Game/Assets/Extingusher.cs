007615043c62b833420958d7a912daf7b7d53241
Ben <bweinberg@brandeis.edu>
2019-10-17 12:13:30 -0600

    Added A Test Scene
    Which combines hands with picking things up and collision

---
+++ VR Serious Game/Assets/Extingusher.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Extingusher : MonoBehaviour
+{
+    float collisionTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision target)
+    {
+        if (target.gameObject.tag.Equals("Pin") == true)
+        {
+            collisionTime = Time.time;
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionStay(Collision target)
+    {
+        if (target.gameObject.tag.Equals("Pin") == true)
+        {
+            if (Time.time - collisionTime > 2)
+            {
+                Destroy(target.gameObject);
+            }
+        }
+    }
+}
