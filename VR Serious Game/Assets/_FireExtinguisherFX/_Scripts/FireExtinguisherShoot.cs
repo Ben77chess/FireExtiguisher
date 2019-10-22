0bb238666401f46c90e16bd2648f070bdff54e09
Anthony Posselli <anthony.posselli@gmail.com>
2019-10-21 23:21:36 -0600

    Add in Lin's models and Jin Jin's particle effects. Begin combining assets.

---
+++ VR Serious Game/Assets/_FireExtinguisherFX/_Scripts/FireExtinguisherShoot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireExtinguisherShoot : MonoBehaviour
+{
+    private ParticleSystem mParticle;
+    private bool isPlaying;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mParticle == null)
+        {
+            mParticle = this.GetComponent<ParticleSystem>();
+            mParticle.Stop(true);
+        }
+
+        isPlaying = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnFireShooting();
+    }
+
+    void OnFireShooting()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            if (isPlaying == false)
+            {
+                isPlaying = true;
+                mParticle.Play(true);
+            }
+        }
+        
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            isPlaying = false;
+            mParticle.Stop(true);
+        }
+    }
+}
