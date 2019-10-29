f74af08360861f55762c49f91c03e09ea7a18620
Ben <bweinberg@brandeis.edu>
2019-10-29 09:06:12 -0600

    Added Scene Reset

---
+++ VR Serious Game/Assets/Scripts/ResetScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponentInChildren<GameObject>().SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider otherCollider)
+    {
+        if (OVRInput.Get(OVRInput.Button.PrimaryThumbstick))
+        {
+            UnityEngine.SceneManagement.Scene scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+            UnityEngine.SceneManagement.SceneManager.LoadScene(scene.name);
+        }
+    }
+}
