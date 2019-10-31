19c1ff0178ad7cd60b2bb624a10a33d028ddd06d
najieyi <nasjieyi@gmail.com>
2019-10-30 19:33:16 -0600

    stage nozzle script

---
+++ VR Serious Game/Assets/Scripts/PostProcessFog.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcessFog : MonoBehaviour
+{
+    private Material mFogMat;
+    public Shader mFogShader;
+
+    [Header("Fog")]
+    public Color f_Color = Color.grey;
+    public float f_bottom = -10.0f;
+    public float f_top = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mFogMat == null)
+        {
+            mFogMat = new Material(mFogShader);
+        }
+        Camera.main.depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Matrix4x4 cameraToWorldMatrix = Camera.main.cameraToWorldMatrix;
+        mFogMat.SetMatrix("_CameraToWorldMatrix", cameraToWorldMatrix);
+        mFogMat.SetColor("_FogColor", f_Color);
+        mFogMat.SetFloat("_FogBottomStart", f_bottom);
+        mFogMat.SetFloat("_FogTopEnd", f_top);
+        Graphics.Blit(source, destination, mFogMat);
+    }
+}
---
+++ VR Serious Game/Assets/Scripts/nozzle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nozzle : MonoBehaviour
+{
+    private OVRGrabbable nozzleGrabbable;
+    private GameObject Grabber;
+    private Vector3 initPositon;
+    private Quaternion initRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nozzleGrabbable = gameObject.GetComponent<OVRGrabbable>();
+        initPositon = transform.localPosition;
+        initRotation = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nozzleGrabbable.isGrabbed && Grabber != null)
+        {
+            gameObject.GetComponent<FixedJoint>().connectedBody = Grabber.GetComponent<Rigidbody>();
+        }
+        else
+        {
+            transform.localPosition = initPositon;
+            transform.localRotation = initRotation;
+            gameObject.GetComponent<FixedJoint>().connectedBody = transform.parent.Find("Body").GetComponent<Rigidbody>();
+        }
+    }
+
+    void OnTriggerStay(Collider otherCollider)
+    {
+        if (otherCollider.gameObject.GetComponentInParent<OVRGrabber>())
+        {
+            Grabber = otherCollider.transform.parent.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider otherCollider)
+    {
+        if (otherCollider.gameObject.GetComponent<OVRGrabber>())
+        {
+            Grabber = null;
+        }
+    }
+}
